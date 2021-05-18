b1a6b4c32b25ce39ba68ddfcf3677c04c2906790
suzuki <s.suzukitime0211@gmail.com>
2021-05-18 13:52:07 +0000

    (start)4-10

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '3466b153a8d42cd17c52b121da10fcf133313078a4ed5aad5f8eb76d25e95ad25232b9ec9c9b1eb8acea6d12af0203504b69de19e88e0f8cec4f484c3ea8be2c'
