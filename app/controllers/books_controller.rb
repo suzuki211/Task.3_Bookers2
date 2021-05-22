4d0ff493d7b541a23b3e7bd6b51137b143d67cc6
suzuki <s.suzukitime0211@gmail.com>
2021-05-22 09:59:05 +0000

    (Update)5/22

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -20,6 +20,10 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
   end
+  
+  def edit
+    @book = Book.find(params[:id])  
+  end
 
   def destroy
     @book = Book.find(params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
-  resources :books, only: [:new, :create, :index, :show, :destroy]
+  resources :books, only: [:new, :create, :index, :show, :edit, :destroy] do
+  end
+  resources :users, only: [:show, :edit, :update]
 end
